0efe9da67eb61c1c9856b12b6132832132a6c3c4
Unknown <idadi70@gmail.com>
2017-03-08 15:01:09 +0700

    ubah siswa

--- application/controllers/Siswa.php
+++ application/controllers/Siswa.php
@@ -3,7 +3,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Siswa extends CI_Controller {
 
-	private $nis;
+	private $nis, $kategori,$pofil;
 
 	public function __construct()
 	{
@@ -13,44 +13,80 @@ class Siswa extends CI_Controller {
 		$this->load->helper(array('url','akses'),'',true);
 		$this->load->model('siswa_model','',true);
 		$this->nis = $this->session->userdata('username');
+		$nis = $this->nis;
+		$this->profil = $this->siswa_model->datasiswa($nis)->row_array();
 		cek_siswa();
 	}
 
-	public function index()
-	{
-		//echo "siswa";
-		//echo "<a href=".base_url('modules/siswa/pilihsoal/1').">mulai ujian</a>";
-		$nis = $this->nis;
-		$data = $this->siswa_model->datasiswa($nis)->row_array();
+	public function index(){
+		$ujian['cek'] = 0;
+		$data = $this->siswa_model->cek_ujian($this->nis)->row_array();
+		if(count($data)){
+			$ujian['cek']=1;
+			$this->session->set_userdata('id_ujian',$data['id_ujian']); //masukkan id ujian yg lama
+		}
 		echo "<a href=".base_url('logout').">logout</a>";
-		$this->load->view('template/header',$data);
-		$this->load->view('siswa/home');
+		$this->load->view('template/header',$this->profil);
+		$this->load->view('siswa/home',$ujian);
 		$this->load->view('template/footer');
 	}
 
-	public function pilihsoal($mapel)
+	public function mulaiujian(){ //method jika mulai ujian yg baru
+		$data = array(
+			'nis' => $this->nis
+			);
+		$id_ujian = $this->siswa_model->input_ujian($data)->row_array();
+		$this->session->set_userdata('id_ujian',$id_ujian['id_ujian']); //masukkan id ujian yg baru
+
+		$kategori = $this->profil['id_kategori'];
+		$mapel = $this->siswa_model->get_mapel($kategori)->result_array();
+
+		foreach($mapel as $map){
+		$data = array(
+			'id_ujian' => $id_ujian['id_ujian'],
+			'id_mapel' => $map['id_mapel'],
+			'status' => 'belum'
+			);
+		$this->siswa_model->insert_status_ujian($data);
+	}
+		redirect('siswa/mapel');
+	}
+
+	public function mapel()
 	{
-		$batas = 1;
+		
+		$nis = $this->nis;
+		$id_ujian = $this->session->userdata('id_ujian');
+		$data = $this->siswa_model->datasiswa($nis)->row_array();
+		$kategori = $data['id_kategori'];
+		$konten['mapel'] = $this->siswa_model->get_mapel($kategori,$id_ujian)->result_array();
+		echo "<a href=".base_url('logout').">logout</a>";
+		$this->load->view('template/header',$data);
+		$this->load->view('siswa/pilihmapel',$konten);
+		$this->load->view('template/footer');
+	}
+
+	public function pilihsoal($mapel, $batas)
+	{
+		$id_ujian = $this->session->userdata('id_ujian');
+		$this->
+		//random dan insert soal
 		$soal = $this->siswa_model->soal($mapel, $batas)->result_array();
-		$id_ujian = 1;
-		var_dump($soal);
-		echo "<br><br>";
+		
 		foreach($soal as $a){
 			$data = array(
 						'id_soal' => $a['id_soal'],
 						'id_ujian' => $id_ujian,
 						'jawab_siswa' => '',
 					);
+			$this->siswa_model->insert_ujian($data);
 
-			//$this->siswa_model->insert_ujian($data);
-
+			//random jawaban per soal
 			$id_jaw = $this->siswa_model->select_id_jawaban($id_ujian)->row_array();
-
 			$pil_jawban = $this->siswa_model->pil_jawaban($a['id_soal'])->result_array();
 			//var_dump($pil_jawban);
-			$numbers = range('A','B');
+			$numbers = range('A','E');
 			shuffle($numbers);
-			
 
 			$i=0;
 			foreach ($pil_jawban as $pil) { 
@@ -60,19 +96,28 @@ class Siswa extends CI_Controller {
 					'id_jawaban' =>$pil['id_jawaban']
 				);	
 				$i++;
-				var_dump($data);
-					echo "<br>";
+		
+				$this->siswa_model->insert_data('pilihan_jawaban_ujian',$data);
 			}
-			
+		
 		
 		}
 
-
-		//redirect(base_url('modules/siswa/ujian'));
+		redirect('siswa/ujian');
 	}
 
-	public function ujian()
-	{
+	public function semua_soal(){
+		$no_ujian = $this->session->userdata('id_ujian');
+		$soal = $this->siswa_model->soal_ujian($no_ujian);
+	}
 
+	public function ujian($no=0)
+	{
+		$data['no'] = $no;
+		
+		echo "<a href=".base_url('logout').">logout</a>";
+		$this->load->view('template/header',$this->profil);
+		$this->load->view('siswa/soal',$data);
+		$this->load->view('template/footer');
 	}	
 }
